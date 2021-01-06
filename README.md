eb248e7dc43fff4846d98449e4b92ede12424ee2
lghiggino <lghiggino@gmail.com>
2021-01-06 08:38:57 -0300

    making it OOP - lets do this

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Tic tac toe game using html css and JavaScript.
 
-The focus here is to use more functional programming, function factories, and the minimum amount of global variables
+Lets make this OOP
---
+++ tic-tac-toe.js
@@ -0,0 +1,108 @@
+const winningArray = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+    
+let ticTacToe = {
+    allSquares: document.querySelectorAll(".square"),
+    makeMark : (e) => {
+        console.log(e.target.id, e.target.innerText, e.target.dataset.value)
+            if (!e.target.innerText) {
+                if(player.humanTurn){
+                    e.target.innerText = player.humanMarker;
+                    e.target.dataset.value = player.humanMarker;
+
+                    victory.checkForVictory();
+                    player.toggle();
+                    
+                } else{
+                    e.target.innerText = player.computerMarker;
+                    e.target.dataset.value = player.computerMarker;
+                    
+                    victory.checkForVictory();
+                    player.toggle();
+                    
+                }  
+            } else{
+                return
+            }    
+        },
+
+
+
+
+
+}
+
+let player = {
+    humanMarker : "x",
+    computerMarker : "o",
+    humanTurn : true,
+    computerTurn : false,
+    toggle : () => {
+        if (player.humanTurn){
+            player.humanTurn = false;
+            player.computerTurn = true;
+        } else {
+            player.computerTurn = false;
+            player.humanTurn = true;
+        }
+    },
+    //setMark não funciona pq não identifica o valor do event target. Repensar
+    setMark : (e, playerTurn) => {
+        if (playerTurn === player.humanTurn){
+            e.target.innerText = player.humanMarker;
+            e.target.dataset.value = player.humanMarker;
+        }else{
+            e.target.innerText = player.computerMarker;
+            e.target.dataset.value = player.computerMarker;
+        }  
+    },
+}
+
+let victory = {
+    checkForVictory: () => {
+        console.log("is there a victory in this situation?")
+        ticTacToe.allSquares.forEach(square => {
+            // square.dataset.value
+            console.log(square.id.slice(1,2), square.dataset.value)
+        })
+    },
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ticTacToe.allSquares.forEach(square => {
+    square.addEventListener("click", ticTacToe.makeMark)
+})
+    
